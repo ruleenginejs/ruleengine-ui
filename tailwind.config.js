0d229d4bc0aa35bd3094dc4f5701e8d51f2a1980
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-01-08 18:48:23 +0300

    rename

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,23 +32,23 @@ module.exports = {
         blue: ['1px solid var(--focus-border)', '-1px'],
       },
       fontSize: {
-        xxs: ['0.5625rem', { lineHeight: '0.9rem' }],
-        xs: ['0.6875rem', { lineHeight: '1rem' }],
-        sm: ['0.8125rem', { lineHeight: '1.1375rem' }]
+        "9sp": ['0.5625rem', { lineHeight: '0.9rem' }],
+        "11sp": ['0.6875rem', { lineHeight: '1rem' }],
+        "13sp": ['0.8125rem', { lineHeight: '1.1375rem' }],
       },
       opacity: {
         38: '0.38',
       },
       borderRadius: {
-        3: '0.1875rem'
+        "3sp": '0.1875rem'
       },
       spacing: {
-        1.6: '0.4375rem',
-        2.1: '0.5625rem',
-        5.1: '1.3125rem',
-        8.7: '2.1875rem',
-        9.1: '2.375rem',
-        28.5: '7.5rem'
+        "7sp": '0.4375rem',
+        "9sp": '0.5625rem',
+        "21sp": '1.3125rem',
+        "35sp": '2.1875rem',
+        "38sp": '2.375rem',
+        "120sp": '7.5rem'
       },
       minWidth: {
         fit: "fit-content"
