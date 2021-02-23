7a701994bd14003bb913886f387cc116ca925d9d
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-02-23 20:22:32 +0300

    sidebar

--- src/components/sidebar/v-sidebar.vue
+++ src/components/sidebar/v-sidebar.vue
@@ -2,6 +2,8 @@
   <div
     class="v-sidebar"
     :class="{
+      'v-sidebar--w-full': wFull,
+      'v-sidebar--h-full': hFull,
       'v-sidebar--rt-border': rtBorder,
       'v-sidebar--lt-border': ltBorder
     }"
@@ -14,6 +16,14 @@
 export default {
   name: "v-sidebar",
   props: {
+    wFull: {
+      type: Boolean,
+      default: false
+    },
+    hFull: {
+      type: Boolean,
+      default: false
+    },
     ltBorder: {
       type: Boolean,
       default: false
