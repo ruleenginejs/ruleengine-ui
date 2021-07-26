573c09dc455fcdab687c75cf1dc2c353032261b3
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-07-26 12:46:05 +0300

    not found

--- src/demo/views/demo-header.vue
+++ src/demo/views/demo-header.vue
@@ -1,9 +1,16 @@
 <template>
   <v-layout w-full h-center class="demo-header">
-    <div class="demo-header__logo">Rule Engine UI</div>
-    <v-space/>
+    <div class="demo-header__logo">
+      <router-link :to="{ name: 'home' }">Rule Engine UI</router-link>
+    </div>
+    <v-space />
     <div class="demo-header__nav">
-      <a href="https://github.com/ruleenginejs/ruleengine-ui-kit-vue" class="demo-external-link" target="_blank">GitHub</a>
+      <a
+        href="https://github.com/ruleenginejs/ruleengine-ui-kit-vue"
+        class="demo-external-link"
+        target="_blank"
+        >GitHub</a
+      >
     </div>
   </v-layout>
 </template>
--- src/demo/views/demo-not-found.vue
+++ src/demo/views/demo-not-found.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Page Not Found</div>
+  <v-layout h-full w-full vertical>
+    <v-layout not-shrink>
+      <demo-header />
+    </v-layout>
+    <v-layout all-distr min-h-0>
+      <v-content class="demo-content" w-full h-full scroll>
+        <p>
+          The page you are visiting was not found. Go to
+          <router-link class="demo-link" :to="{ name: 'home' }">main page.</router-link>
+        </p>
+      </v-content>
+    </v-layout>
+  </v-layout>
 </template>
 
 <script>
+import DemoHeader from "./demo-header";
+
 export default {
-  name: "demo-not-found"
+  name: "demo-not-found",
+  components: {
+    DemoHeader
+  }
 };
 </script>
