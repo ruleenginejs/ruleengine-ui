de283ed9718931d04f5d7f6e73ecc31eb649d7c0
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-07-31 22:33:15 +0300

    render content after split view created

--- src/demo/views/demo-component-layout.vue
+++ src/demo/views/demo-component-layout.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-split-view :snap-offset="30">
+  <v-split-view :snap-offset="30" @created="created = true">
     <v-split-pane size="290px">
       <demo-sidebar />
     </v-split-pane>
     <v-split-pane :min-size="200">
-      <v-content class="demo-content" w-full h-full scroll>
+      <v-content v-if="created" class="demo-content" w-full h-full scroll>
         <router-view />
       </v-content>
     </v-split-pane>
@@ -12,12 +12,18 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import DemoSidebar from "./demo-sidebar";
 
 export default {
   name: "demo-component-layout",
   components: {
     DemoSidebar
+  },
+  setup() {
+    return {
+      created: ref(false)
+    };
   }
 };
 </script>
