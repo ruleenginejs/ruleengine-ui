0a73ab873f24ab16a31dde598dcc3c559a957a9d
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-07-31 23:15:35 +0300

    action-bar demo

---
+++ src/demo/views/components/demo-component-action-bar.vue
@@ -0,0 +1,85 @@
+<template>
+  <h2>ActionBar</h2>
+
+  <h3>Basic Usage</h3>
+  <demo-block>
+    <v-action-bar>
+      <v-action @click="onClick">
+        <v-icon-play />
+      </v-action>
+      <v-action @click="onClick">
+        <v-icon-stop />
+      </v-action>
+      <v-action @click="onClick">
+        <v-icon-plus />
+      </v-action>
+      <v-action @click="onClick">
+        <v-icon-folder />
+      </v-action>
+    </v-action-bar>
+    <template #code>
+      <demo-code name="actionbar_basic_usage_html" />
+    </template>
+  </demo-block>
+
+  <h3>Disabled Action</h3>
+  <demo-block>
+    <v-action-bar>
+      <v-action @click="onClick">
+        <v-icon-play />
+      </v-action>
+      <v-action disabled @click="onClick">
+        <v-icon-stop />
+      </v-action>
+    </v-action-bar>
+    <template #code>
+      <demo-code name="actionbar_disabled_action_usage_html" />
+    </template>
+  </demo-block>
+
+  <h3>Grouping Action</h3>
+  <demo-block>
+    <v-action-bar>
+      <v-action-group>
+        <v-action @click="onClick">
+          <span class="codicon codicon-play" />
+        </v-action>
+        <v-action @click="onClick">
+          <span class="codicon codicon-stop" />
+        </v-action>
+      </v-action-group>
+      <v-action @click="onClick">
+        <span class="codicon codicon-plus" />
+      </v-action>
+      <v-action-group>
+        <v-action @click="onClick">
+          <span class="codicon codicon-zoom-in" />
+        </v-action>
+        <v-action @click="onClick">
+          <span class="codicon codicon-zoom-out" />
+        </v-action>
+      </v-action-group>
+    </v-action-bar>
+    <template #code>
+      <demo-code name="actionbar_grouping_action_usage_html" />
+    </template>
+  </demo-block>
+</template>
+
+<script>
+import DemoBlock from "../demo-block";
+import DemoCode from "../demo-code";
+
+export default {
+  name: "demo-component-action-bar",
+  components: {
+    DemoBlock,
+    DemoCode
+  },
+  setup() {
+    return {
+      onClick: () => {}
+    };
+  }
+};
+</script>
--- src/demo/views/demo-codes/index.js
+++ src/demo/views/demo-codes/index.js
@@ -1,39 +1,13 @@
-import componentAutocomplete from "./component-autocomplete";
-import componentButton from "./component-button";
-import componentCheckbox from "./component-checkbox";
-import componentContent from "./component-content";
-import componentFieldset from "./component-fieldset";
-import componentIcon from "./component-icon";
-import componentInput from "./component-input";
-import componentInstallation from "./component-installation";
-import componentLabel from "./component-label";
-import componentLayout from "./component-layout";
-import componentList from "./component-list";
-import componentScrollbar from "./component-scrollbar";
-import componentSelectbox from "./component-selectbox";
-import componentSidebar from "./component-sidebar";
-import componentSpace from "./component-space";
-import componentSplitview from "./component-splitview";
-import componentTabs from "./component-tabs";
-import componentTreeview from "./component-treeview";
+const requireComponent = require.context(
+  "@/demo/views/demo-codes",
+  true,
+  /component-[a-z0-9-]*.(js)$/
+);
 
-export default [
-  ...componentInstallation,
-  ...componentLayout,
-  ...componentButton,
-  ...componentSpace,
-  ...componentScrollbar,
-  ...componentIcon,
-  ...componentInput,
-  ...componentContent,
-  ...componentList,
-  ...componentSidebar,
-  ...componentTreeview,
-  ...componentCheckbox,
-  ...componentSelectbox,
-  ...componentFieldset,
-  ...componentLabel,
-  ...componentAutocomplete,
-  ...componentSplitview,
-  ...componentTabs
-]
+const codes = [];
+
+requireComponent.keys().forEach(fileName => {
+  codes.push.apply(codes, requireComponent(fileName).default);
+});
+
+export default codes;
