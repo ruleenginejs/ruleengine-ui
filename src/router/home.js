3f49efd22638baef1355c704ea582bec3af3dce6
ruleenginejs <76452182+ruleenginejs@users.noreply.github.com>
2021-08-02 14:59:58 +0300

    Update home.js

--- src/router/home.js
+++ src/router/home.js
@@ -1,7 +1,7 @@
 import component from "./component";
 
 const DemoLayout = () => import("@/demo/views/demo-layout");
-const DemoHome = () => import("@/demo/views//demo-home");
+const DemoHome = () => import("@/demo/views/demo-home");
 
 export default [
   {
