8cb3b2e4f4cc311c52c8fcb02f1c62e137f43940
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-11-12 22:49:08 +0300

    Add vertical action list

--- src/components/action-list/v-action-list.vue
+++ src/components/action-list/v-action-list.vue
@@ -1,11 +1,27 @@
 <script>
 export default {
   name: "v-action-list"
-}
+};
+</script>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  vertical: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const cssClasses = computed(() => ({
+  "v-action-list--horizontal": !props.vertical,
+  "v-action-list--vertical": props.vertical
+}));
 </script>
 
 <template>
-  <div class="v-action-list">
+  <div class="v-action-list" :class="cssClasses">
     <slot></slot>
   </div>
 </template>
