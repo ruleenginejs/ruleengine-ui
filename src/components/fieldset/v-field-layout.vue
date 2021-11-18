7729e559d7feaf26fc7eb5fb26e84a95ce6415df
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-11-18 20:10:53 +0300

    Add top prop

--- src/components/fieldset/v-field-layout.vue
+++ src/components/fieldset/v-field-layout.vue
@@ -1,14 +1,10 @@
 <template>
-  <div
-    v-if="$slots.label || $slots.value"
-    class="v-field-layout"
-    :class="cssClasses"
-  >
+  <div v-if="$slots.label || $slots.value" class="v-field-layout" :class="cssClasses">
     <div v-if="$slots.label" class="v-field-layout__label">
-      <slot name="label" />
+      <slot name="label"></slot>
     </div>
     <div v-if="$slots.value" class="v-field-layout__value">
-      <slot name="value" />
+      <slot name="value"></slot>
     </div>
   </div>
 </template>
@@ -22,14 +18,19 @@ export default {
     vertical: {
       type: Boolean,
       default: false
+    },
+    top: {
+      type: Boolean,
+      default: false
     }
   },
   setup(props) {
-    const { vertical } = toRefs(props);
+    const { vertical, top } = toRefs(props);
 
     const cssClasses = computed(() => ({
       "v-field-layout--horizontal": !vertical.value,
-      "v-field-layout--vertical": vertical.value
+      "v-field-layout--vertical": vertical.value,
+      "v-field-layout--top": top.value
     }));
 
     return {
