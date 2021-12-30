680ca0d7e891d0fb8982b9ba2c5312fb7a2fcafc
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-12-30 17:45:31 +0300

    Add new header colors

--- src/components/graph/composables/use-color-preset.js
+++ src/components/graph/composables/use-color-preset.js
@@ -2,15 +2,28 @@ import { computed } from "vue";
 
 const COLOR_PRESET = [
   "blue",
-  "green"
+  "green",
+  "brown",
+  "indigo",
+  "orange",
+  "pink",
+  "purple",
+  "red",
+  "teal",
+  "yellow"
 ];
+
+const COLOR_PRESET_MAP = COLOR_PRESET.reduce((res, val) => {
+  res[val] = true;
+  return res;
+}, {});
 
 export default function useColorPreset(color) {
   const colorStyle = computed(() =>
-    COLOR_PRESET.indexOf(color.value) === -1 ? color.value : null);
+    !COLOR_PRESET_MAP[color.value] ? color.value : null);
 
   const colorCssClass = computed(() =>
-    COLOR_PRESET.indexOf(color.value) !== -1 ? getColorCss(color.value) : null);
+    COLOR_PRESET_MAP[color.value] ? getColorCss(color.value) : null);
 
   function getColorCss(colorName) {
     return `v-graph-node__header--${colorName}`;
